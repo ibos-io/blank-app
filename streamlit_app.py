d37657f0a9ae7bfa8247e894cf54a1b9aabb21e9
ibos-io <suraiaafrozmaria@ibos.io>
2026-06-03 06:50:17 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import numpy as np
 import cv2
 
 st.title("🎈 My new app")
+if st.button("Send balloons!"):
+    st.balloons()
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
